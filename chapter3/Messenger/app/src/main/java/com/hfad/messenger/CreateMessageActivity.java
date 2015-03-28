7f0e6330e18fcbb261b6287ce742e91b87ce2b4d
David Griffiths <dogriffiths@gmail.com>
2015-03-28 20:44:22 +0000

    Switched to a chooser. Got to the end of chapter 3

--- chapter3/Messenger/app/src/main/java/com/hfad/messenger/CreateMessageActivity.java
+++ chapter3/Messenger/app/src/main/java/com/hfad/messenger/CreateMessageActivity.java
@@ -21,6 +21,8 @@ public class CreateMessageActivity extends Activity {
         Intent intent = new Intent(Intent.ACTION_SEND);
         intent.setType("text/plain");
         intent.putExtra(Intent.EXTRA_TEXT, messageText);
-        startActivity(intent);
+        String chooserTitle = getString(R.string.chooser);
+        Intent chosenIntent = Intent.createChooser(intent, chooserTitle);
+        startActivity(chosenIntent);
     }
 }
