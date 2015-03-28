86b5da82e2754286b829579f9feb3b83bdcbb855
David Griffiths <dogriffiths@gmail.com>
2015-03-28 20:40:01 +0000

    Switched to an action

--- chapter3/Messenger/app/src/main/java/com/hfad/messenger/CreateMessageActivity.java
+++ chapter3/Messenger/app/src/main/java/com/hfad/messenger/CreateMessageActivity.java
@@ -18,8 +18,9 @@ public class CreateMessageActivity extends Activity {
     public void onSendMessage(View view) {
         EditText messageView = (EditText)findViewById(R.id.message);
         String messageText = messageView.getText().toString();
-        Intent intent = new Intent(this, ReceiveMessageActivity.class);
-        intent.putExtra(ReceiveMessageActivity.EXTRA_MESSAGE, messageText);
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_TEXT, messageText);
         startActivity(intent);
     }
 }
