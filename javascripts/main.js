7210411110ab5204be0b2ffd6b4aa1023cec4ae8
David Griffiths <dogriffiths@gmail.com>
2015-03-29 21:17:22 +0100

    Added an intro to the book section.

--- javascripts/main.js
+++ javascripts/main.js
@@ -3,9 +3,8 @@ $(function() {
     $(".figure").addClass("col-lg-4");
     $(".outline-2").addClass("row");
     $(".outline-3").addClass(gridSizes);
-//    $(".outline-3 .figure img").css('width', '200%').css('float', 'left').css('border-width', '1px').css('border-color', 'red').css('border-style', 'solid');
+    $(".outline-text-2 .figure img").addClass('side_image');
     $(".outline-3 .figure img").css('max-width', '300px');
-//    $(".outline-3 .figure img").addClass('img-responsive');
     $(".outline-3 a").addClass('btn').addClass('btn-success');
     $(".outline-3").addClass("panel panel-default").css('border-width', '0px').css('box-shadow', 'none');
     $(".outline-3 h3").addClass("panel-heading");
