9559fe1652bb14d69959d1d56d9dce5026e797cf
David Griffiths <dogriffiths@gmail.com>
2015-03-29 20:48:56 +0100

    Moved output to org-mode

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,13 @@
-console.log('This would be the main JS file.');
+$(function() {
+    var gridSizes = "col-xs-12 col-sm-12 col-md-6 col-lg-4";
+    $(".figure").addClass("col-lg-4");
+    $(".outline-2").addClass("row");
+    $(".outline-3").addClass(gridSizes);
+//    $(".outline-3 .figure img").css('width', '200%').css('float', 'left').css('border-width', '1px').css('border-color', 'red').css('border-style', 'solid');
+    $(".outline-3 .figure img").css('max-width', '300px');
+//    $(".outline-3 .figure img").addClass('img-responsive');
+    $(".outline-3 a").addClass('btn').addClass('btn-success');
+    $(".outline-3").addClass("panel panel-default");
+    $(".outline-3 h3").addClass("panel-heading");
+    $(".outline-text-3").addClass("panel-body");
+})
