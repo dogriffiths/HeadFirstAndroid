3a4a6b982d65e5403ebf83eb6193fa9352f756af
David Griffiths <dogriffiths@gmail.com>
2015-03-31 13:08:18 +0100

    Fixed a gappy image.

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,6 +1,6 @@
 $(function() {
     var gridSizes = "col-xs-12 col-sm-12 col-md-6 col-lg-4";
-    $(".figure").addClass("col-lg-4");
+    $(".outline-3 .figure").addClass("col-lg-4");
     $(".outline-2").addClass("row");
     $(".outline-3").addClass(gridSizes);
     $(".outline-text-2 .figure img").addClass('side_image');
