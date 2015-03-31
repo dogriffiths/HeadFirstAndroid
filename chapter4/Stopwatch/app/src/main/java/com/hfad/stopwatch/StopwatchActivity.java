6d3e76777b04a570c87981f4b5e6752a9da87f52
David Griffiths <dogriffiths@gmail.com>
2015-03-31 20:10:07 +0100

    Up to Test Drive on page 16.

--- chapter4/Stopwatch/app/src/main/java/com/hfad/stopwatch/StopwatchActivity.java
+++ chapter4/Stopwatch/app/src/main/java/com/hfad/stopwatch/StopwatchActivity.java
@@ -1,39 +1,57 @@
 package com.hfad.stopwatch;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.os.Handler;
+import android.app.Activity;
+import android.view.View;
+import android.widget.TextView;
 
-
-public class StopwatchActivity extends ActionBarActivity {
+public class StopwatchActivity extends Activity {
+    //Number of seconds displayed on the stopwatch.
+    private int seconds = 0;
+    //Is the stopwatch running?
+    private boolean running;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stopwatch);
+        runTimer();
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_stopwatch, menu);
-        return true;
+    //Start the stopwatch running when the Start button is clicked.
+    public void onClickStart(View view) {
+        running = true;
     }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    
+    //Stop the stopwatch running when the Stop button is clicked.
+    public void onClickStop(View view) {
+        running = false;
+    }
+    
+    //Reset the stopwatch when the Reset button is clicked.
+    public void onClickReset(View view) {
+        running = false;
+        seconds = 0;
+    }
+    
+    //Sets the number of seconds on the timer.
+    private void runTimer() {
+        final TextView timeView = (TextView)findViewById(R.id.time_view);
+        final Handler handler = new Handler();
+        handler.post(new Runnable() {
+                @Override
+                public void run() {
+                    int hours = seconds/3600;
+                    int minutes = (seconds%3600)/60;
+                    int secs = seconds%60;
+                    String time = String.format("%d:%02d:%02d", hours, minutes, secs);
+                    timeView.setText(time);
+                    if (running) {
+                        seconds++;
+                    }
+                    handler.postDelayed(this, 1000);
+                }
+            });
     }
 }
