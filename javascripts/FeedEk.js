91720001983767faa4fae2a569b3607eb13518df
David Griffiths <dogriffiths@gmail.com>
2015-03-31 22:25:03 +0100

    Tweaked protocol in feed code

--- javascripts/FeedEk.js
+++ javascripts/FeedEk.js
@@ -21,7 +21,7 @@
         $("#" + id).empty().append('<img src="loader.gif" />');
 
         $.ajax({
-            url: "http://ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=" + def.MaxCount + "&output=json&q=" + encodeURIComponent(def.FeedUrl) + "&hl=en&callback=?",
+            url: "//ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=" + def.MaxCount + "&output=json&q=" + encodeURIComponent(def.FeedUrl) + "&hl=en&callback=?",
             dataType: "json",
             success: function (data) {
                 $("#" + id).empty();
